4ca298edf00752aaffeebeb423a4df6565869c0d
Lourdes Avalos <lulyavalosmartinez@gmail.com>
2023-11-23 14:34:50 -0300

    partes de main

--- src/assets/Components/Main.jsx
+++ src/assets/Components/Main.jsx
@@ -1,28 +1,61 @@
+
 import "./style/Main.css"
-function Main () {
+
+function Main() {
+
+
+
     return (
-    <>
-    <h1>Aprendiendo a Programar</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, culpa.</p>
-    
-    <form action="">
-        <label htmlFor="">Nombre y Apellido</label>
-        <input type="text" />
-        <label htmlFor="">Email</label>
-        <input type="text" />
-        <select name="" id="">
-            <option value="">JavaScrip</option>
-            <option value="">HTML5</option>
-            <option value="">CSS3</option>
-        </select>
-        <textarea name="" id="" cols="30" rows="10">Mensaje</textarea>
-        <button>Eviar mensaje</button>
-    </form>
-    
-    <div>
-    <img src="././img/Img1.svg" alt="img de la pàgina" />
-    </div>
-    </>
+        <div className="container-principal">
+            <div className="container-form">
+                <h1 className="title">Aprendiendo a Programar</h1>
+                <p className="subtitle">Aprenda a diseñar y crear páginas web. Construya una cartera web receptiva y accesible utilizando HTML5, CSS3 y JavaScript</p>
+
+                <form className="form" action="">
+
+                    {/* nombre */}
+                    <div className="form-input">
+                        <label htmlFor="nombre">Nombre y Apellido</label>
+                        <input className="input-container" 
+                        type="text" 
+                        name="nombre"
+                        required
+
+                        />
+                    </div>
+
+                    {/* email */}
+                    <div className="form-input">
+                        <label htmlFor="email">Email</label>
+                        <input className="input-container" 
+                        type="text" 
+                        required 
+                        autoComplete="email"
+                        />
+                    </div>
+
+                    <div className="form-input">
+                        <label htmlFor="cursos"> Cursos disponibles</label>
+                        <select name="" id="select">
+                            <option value="javascrip">JavaScrip</option>
+                            <option value="html5">HTML5</option>
+                            <option value="css3">CSS3</option>
+                        </select>
+                    </div>
+                    <div className="form-input">
+                        <label htmlFor="">Dejanos tu consulta</label>
+                        <textarea id="text-mensaje" cols="30" rows="10" placeholder="Mensaje"></textarea>
+                    </div>
+
+                    <button className="btn">Eviar mensaje</button>
+                </form>
+            </div>
+
+
+            <div className="img-container">
+                <img className="img-principal" src="././img/img1.png" alt="img de la pàgina" />
+            </div>
+        </div>
     )
 }
 
